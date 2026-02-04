8d0c3d448300eb28e30826927b29b8b399a5dc81
Kyle <hyrokai.oliver@gmail.com>
2026-02-04 11:06:16 +0100

    changes

--- src/model/Caballo.java
+++ src/model/Caballo.java
@@ -8,12 +8,10 @@ public class Caballo extends Pieza {
 
     @Override
     public boolean validoMovimiento(Movimiento mov, Tablero tablero) {
-        // Get absolute differences (positive values)
-        int deltaFila = Math.abs(mov.saltoVertical());    // Change in row
-        int deltaColumna = Math.abs(mov.saltoHorizontal()); // Change in column
 
-        // Knight moves 2+1 or 1+2
-        // One of these must be 2 and the other must be 1
+        int deltaFila = Math.abs(mov.saltoVertical());
+        int deltaColumna = Math.abs(mov.saltoHorizontal());
+
         boolean movimientoValido = (deltaFila == 2 && deltaColumna == 1) ||
                 (deltaFila == 1 && deltaColumna == 2);
 
@@ -21,9 +19,8 @@ public class Caballo extends Pieza {
             return false;
         }
 
-        // Check destination - Knights can jump over pieces!
-        // Only check if destination has a piece of the SAME color
-        Posicion destino = mov.getPosFinal(); // You need this getter in Movimiento
+
+        Posicion destino = mov.getPosFinal();
         Pieza piezaDestino = tablero.devuelvePieza(destino);
 
         if (piezaDestino != null && piezaDestino.getColor().equals(this.getColor())) {
